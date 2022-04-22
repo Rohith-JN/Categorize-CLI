9f162c8ef952c65366061b4d1db12d769f772f06
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-22 23:00:05 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -8,25 +8,33 @@ with open("README.md", "r") as fh:
 setup(
 
     name='Categorize-CLI',
-    version='0.5.0',
-    description='Categorize-CLI is a command-line-tool made using python to organize files in a given directory',
+    version='1.0.0',
+    description='Categorize-CLI is a command-line-tool made to help you categorize/organize files in a given directory',
     url="https://github.com/Rohith-JN/Categorize-CLI",
     author="Rohith Nambiar",
     packages=find_packages(exclude="tests"),
     include_package_data=True,
+    license= "MIT",
     author_email='rohithnambiar04@gmail.com',
     classifiers=[
         'Development Status :: 4 - Beta',
         "License :: OSI Approved :: MIT License",
         'Intended Audience :: End Users/Desktop',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.10',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         'Operating System :: Microsoft :: Windows'
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     project_urls={
-        "Bug Tracker": "https://github.com/Rohith-JN/Categorize-CLI/issues"
+        "Changelog" : "https://github.com/Rohith-JN/Categorize-CLI/CHANGELOG.md",
+        "Issues": "https://github.com/Rohith-JN/Categorize-CLI/issues",
+        "Source" : "https://github.com/Rohith-JN/Categorize-CLI"
     },
     install_requires=['Click', 'colorama', 'progress'],
 
