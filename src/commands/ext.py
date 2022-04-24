923a35e005c1d16b11f9743ae5709b5c373f3b96
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-24 22:08:26 +0530

    Added commands

---
+++ src/commands/ext.py
@@ -0,0 +1,27 @@
+import click
+from src.common.extensions import extensions
+from src.services.ext_functions import *
+
+class Context:
+    def __init__(self, type, path):
+        self.type = type
+        self.path = path
+
+@click.command()
+@click.option("-t", "--type", type=str, help = "Type of extension", required=True)
+@click.option("-p", "--path", type=str, help = "Path to organize", required=False, default=os.getcwd())
+@click.pass_context
+def main(ctx, type, path):
+    """Organize files based on specified extension"""
+    ctx.obj = Context(type, path)
+    extension = ctx.obj.type
+    folder_to_track = ctx.obj.path
+
+    categories = [key for key, value in extensions.items()]
+    if extension in categories and os.path.exists(folder_to_track):
+        click.echo(extension_category(extensions[extension], folder_to_track))
+    elif not extension in categories:
+        click.echo("{} is not one of the types of extensions".format(extension))
+    elif not os.path.exists(folder_to_track):
+        click.echo("{}: does not exist".format(folder_to_track))
+
---
+++ src/commands/key.py
@@ -0,0 +1,23 @@
+import click
+from src.services.key_functions import *
+
+class Context:
+    def __init__(self, keyword, path):
+        self.keyword = keyword
+        self.path = path
+
+@click.command()
+@click.option("-k", "--keyword", type=str, help = "Keyword to categorize files", required=True)
+@click.option("-p", "--path", type=str, help = "Path to organize", required=False, default=os.getcwd())
+@click.pass_context
+def main(ctx, keyword, path):
+    """Organize files based on specified keyword"""
+    ctx.obj = Context(keyword, path)
+    keyword = ctx.obj.keyword
+    folder_to_track = ctx.obj.path
+
+    if os.path.exists(folder_to_track):
+        click.echo(specific_name_category(keyword, folder_to_track))
+    else:
+        click.echo("{}: does not exist".format(folder_to_track))
+    
