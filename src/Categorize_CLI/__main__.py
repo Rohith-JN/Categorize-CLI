30136adaee454ba81f6059ab66eedbc2ac965fc0
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-25 10:01:38 +0530

    Modified __main__.py

--- src/Categorize_CLI/__main__.py
+++ src/Categorize_CLI/__main__.py
@@ -1,10 +1,10 @@
 import os
 import click
 
-from common import extensions
-from services.ext_functions import *
-from services.key_functions import *
-from services.year_functions import *
+from src.Categorize_CLI.common.extensions import *
+from src.Categorize_CLI.services.ext_functions import *
+from src.Categorize_CLI.services.key_functions import *
+from src.Categorize_CLI.services.year_functions import *
  
 
 class Context:
@@ -15,84 +15,15 @@ class Context:
         self.year = year
         self.path = path
 
-class ComplexCLI(click.MultiCommand):
-    def list_commands(self, ctx):
-        commands = []
-        commands_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands"))
-        for filename in os.listdir(commands_folder):
-            if filename.endswith(".py") and not filename.startswith("__"):
-                commands.append(filename.replace(".py", ""))
-
-        commands.sort()
-        return commands
-
-    def get_command(self, ctx, name):
-        try:
-            mod = __import__(f"src.Categorize_CLI.commands.{name}", None, None, ["main"])
-        except ImportError:
-            return
-        return mod.main
-
-@click.pass_context
-def ext_all(ctx, all):
-    """Organize all files based on extension"""
-    ctx.obj = Context(all, path)
-    all = ctx.obj.all
-    path = ctx.obj.path
-
-    if os.path.exists(path) and all:
-        click.echo(all_extensions_category(path))
-    else:
-        click.echo("{}: does not exist".format(path))
-
-@click.pass_context
-def ext(ctx, type, path):
-    """Organize files based on specified extension"""
-    ctx.obj = Context(type, path)
-    type = ctx.obj.type
-    path = ctx.obj.path
-
-    categories = [key for key, value in extensions.items()]
-    if type in categories and os.path.exists(path):
-        click.echo(extension_category(extensions[type], path))
-    elif not type in categories:
-        click.echo("{} is not one of the types of extensions".format(type))
-    elif not os.path.exists(path):
-        click.echo("{}: does not exist".format(path))
-
-@click.pass_context
-def key(ctx, keyword, path):
-    """Organize files based on specified keyword"""
-    ctx.obj = Context(keyword, path)
-    keyword = ctx.obj.keyword
-    path = ctx.obj.path
-
-    if os.path.exists(path):
-        click.echo(specific_name_category(keyword, path))
-    else:
-        click.echo("{}: does not exist".format(path))
-
-@click.pass_context
-def year(ctx, year, path):
-    """Organize files based on year created"""
-    ctx.obj = Context(year, path)
-    year = ctx.obj.year
-    path = ctx.obj.path
-
-    if os.path.exists(path) and year:
-        click.echo(year_category(path))
-    else:
-        click.echo("{}: does not exist".format(path))
-    
-
 @click.command()
-@click.option("-t", "--type", type=str, help = "Type of extension", required=False, callback=ext)
-@click.option("-a", "--all", is_flag = True, help = "Organize all files based on keyword", required = False, callback=ext_all)
-@click.option("-k", "--keyword", type=str, help = "Keyword to categorize files", required=False, callback=key)
-@click.option("-y", "--year", is_flag = True, help = "Organize files based on year", required=False, callback=year)
+@click.option("-t", "--type", type=str, help = "Type of extension", required=False)
+@click.option("-a", "--all", is_flag = True, help = "Organize all files based on keyword", required = False)
+@click.option("-k", "--keyword", type=str, help = "Keyword to categorize files", required=False)
+@click.option("-y", "--year", is_flag = True, help = "Organize files based on year", required=False)
 @click.option("-p", "--path", type=str, help = "Path to organize", required=False, default=os.getcwd())
 @click.pass_context
 def main(ctx, type, all, keyword, year, path):
     """Welcome to Categorize!"""
 
+    
 
