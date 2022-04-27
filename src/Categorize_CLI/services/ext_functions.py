21689d1bef3c0d3f408661c6e6af680cb11fc863
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-27 22:51:33 +0530

    Formatted output statement

--- src/Categorize_CLI/services/ext_functions.py
+++ src/Categorize_CLI/services/ext_functions.py
@@ -7,14 +7,13 @@ from colorama import Fore
 
 from src.Categorize_CLI.common.secondary_functions import *
 
-
 init()
 
 def all_extensions_category(folder_to_track, verbose):
     moved = {}
     movedList = []
-    size = 0
     no_of_files_moved = 0
+    size = 0
     movedFiles = False
     count = 0
     start_time = time.monotonic()
@@ -63,10 +62,8 @@ def all_extensions_category(folder_to_track, verbose):
 
                     output = '\n'.join(map(str, movedList))
 
-                    print(os.linesep)
-                    displayProgressbar(count)
-
                     if movedFiles:
+                        displayProgressbar(count)
                         if count == 1 and verbose:
                             return f"Successfully moved {count} file{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}{os.linesep}Files Moved:{os.linesep}{output}{os.linesep}"
                         elif count != 1 and verbose:
@@ -79,17 +76,24 @@ def all_extensions_category(folder_to_track, verbose):
                             return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
 
                     else:
-                        return os.linesep + Fore.RED + f'{folder_to_track}: is either empty or not organizable' + os.linesep
+                        return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
 
                 except Exception as e:
-                    return os.linesep + Fore.RED + f'{folder_to_track}: is either empty or not organizable' + os.linesep
+                    return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
 
-    else:
-        return os.linesep+ Fore.RED + f'{folder_to_track}: is either empty or not organizable' + os.linesep
+    elif os.listdir(folder_to_track).__len__() == 0:
+        return os.linesep + Fore.RED + f'Error: {folder_to_track} is empty' + os.linesep
+    elif all(os.path.isdir(os.path.join(folder_to_track, file + '//')) for file in os.listdir(folder_to_track)):
+        return os.linesep + Fore.RED + f'Error: {folder_to_track} only contains folders' + os.linesep
+    else: 
+        return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
 
 
-def extension_category(extension, folder_to_track):
+def extension_category(extension, folder_to_track, verbose):
     start_time = time.monotonic()
+    moved = {}
+    movedList = []
+    no_of_files_moved = 0
     size = 0
     movedFiles = False
     count = 0
@@ -101,9 +105,9 @@ def extension_category(extension, folder_to_track):
                     for filename in os.listdir(folder_to_track):
                         for value in extension:
                             if not os.path.isdir(os.path.join(folder_to_track, filename)) and any(
-                                    filename.endswith(value) for filename in os.listdir(folder_to_track)) == True:
+                                    filename.endswith(value) for filename in os.listdir(folder_to_track)):
                                 file_mappings.setdefault(get_key(extension), []).append(filename)
-
+                                
                     for folder_name, folder_items in file_mappings.items():
                         folder_path = os.path.join(folder_to_track, folder_name)
                         folder_exists = os.path.exists(folder_path)
@@ -112,45 +116,57 @@ def extension_category(extension, folder_to_track):
 
                             for filename in os.listdir(folder_to_track):
                                 for value in extension:
-                                    if not os.path.isdir(os.path.join(folder_to_track, filename)) and filename.endswith(
-                                            value):
+                                    if not os.path.isdir(os.path.join(folder_to_track, filename)) and filename.endswith(value):
                                         count = count + 1
                                         source = os.path.join(folder_to_track, filename)
                                         size = size + os.path.getsize(source)
                                         destination = os.path.join(folder_path, filename)
+                                        moved[filename] = folder_name + '/' + filename
                                         moveIncrementing(source,destination)
                                         movedFiles = True
 
                         if folder_exists:
                             for filename in os.listdir(folder_to_track):
                                 for value in extension:
-                                    if not os.path.isdir(os.path.join(folder_to_track, filename)) and filename.endswith(
-                                            value):
+                                    if not os.path.isdir(os.path.join(folder_to_track, filename)) and filename.endswith(value):
                                         count = count + 1
                                         source = os.path.join(folder_to_track, filename)
                                         size = size + os.path.getsize(source)
                                         destination = os.path.join(folder_path, filename)
-                                        moveIncrementing(source,
-                                                         destination)
+                                        moveIncrementing(source, destination)
+                                        moved[filename] = folder_name + '/' + filename
                                         movedFiles = True
 
                     end_time = time.monotonic()
 
-                    displayProgressbar(count)
+                    for key, value in moved.items():
+                        no_of_files_moved = no_of_files_moved + 1
+                        movedList.append(f"{no_of_files_moved}) {key} --> {value}")
+
+                    output = '\n'.join(map(str, movedList))
 
                     if movedFiles:
-                        if count == 1:
+                        displayProgressbar(count)
+                        if count == 1 and verbose:
+                            return f"Successfully moved {count} file{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}{os.linesep}Files Moved:{os.linesep}{output}{os.linesep}"
+                        elif count != 1 and verbose:
+                            return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}{os.linesep}Files Moved:{os.linesep}{output}{os.linesep}"
+                        elif count == 1 and not verbose:
                             return f"Successfully moved {count} file{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
-                        elif count != 1:
+                        elif count != 1 and not verbose:
                             return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
                         else:
                             return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
 
                     else:
-                        return "Files with that extension do not exist in {}".format(folder_to_track)
+                        return os.linesep + Fore.RED + f'Error: {folder_to_track} does not contain any {get_key(extension)} files' + os.linesep
 
                 except Exception as e:
-                    return f'{folder_to_track}: is either empty or not organizable'
+                    return os.linesep + Fore.RED + f'Error: {folder_to_track} does not contain any {get_key(extension)} files' + os.linesep
 
-    else:
-        return f'{folder_to_track}: is either empty or not organizable'
+    elif os.listdir(folder_to_track).__len__() == 0:
+        return os.linesep + Fore.RED + f'Error: {folder_to_track} is empty' + os.linesep
+    elif all(os.path.isdir(os.path.join(folder_to_track, file + '//')) for file in os.listdir(folder_to_track)):
+        return os.linesep + Fore.RED + f'Error: {folder_to_track} only contains folders' + os.linesep
+    else: 
+        return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
