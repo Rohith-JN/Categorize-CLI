adbf923ee3b3a5f0f48acbc710d152191475eb4a
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-28 16:12:52 +0530

    Added verbose option

--- src/Categorize_CLI/commands/year.py
+++ src/Categorize_CLI/commands/year.py
@@ -1,20 +1,27 @@
 import click
 from src.Categorize_CLI.services.year_functions import *
+from colorama import init
+from colorama import Fore
+
+init()
 
 class Context:
-    def __init__(self, path):
+    def __init__(self, path, verbose):
         self.path = path
+        self.verbose = verbose
 
 @click.command()
 @click.option("-p", "--path", type=str, help = "Path to organize", required=False, default=os.getcwd())
+@click.option("-v", "--verbose", is_flag = True, help = "Show complete output", required = False, default = False)
 @click.pass_context
-def main(ctx,path):
+def main(ctx, path, verbose):
     """Organize files based on year created"""
-    ctx.obj = Context(path)
+    ctx.obj = Context(path, verbose)
     folder_to_track = ctx.obj.path
+    verbose = ctx.obj.verbose
 
     if os.path.exists(folder_to_track):
-        click.echo(year_category(folder_to_track))
-    else:
-        click.echo("{}: does not exist".format(folder_to_track))
+        click.echo(year_category(folder_to_track, verbose))
+    elif not os.path.exists(folder_to_track):
+        click.echo(os.linesep + Fore.RED + "Error: {} does not exist".format(folder_to_track) + os.linesep)
     
