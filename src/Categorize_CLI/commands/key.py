513b1467d0bad8512292dd82d9446891b776edc0
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-28 16:12:37 +0530

    Added verbose option

--- src/Categorize_CLI/commands/key.py
+++ src/Categorize_CLI/commands/key.py
@@ -1,23 +1,31 @@
 import click
 from src.Categorize_CLI.services.key_functions import *
+from colorama import init
+from colorama import Fore
+
+init()
 
 class Context:
-    def __init__(self, keyword, path):
+    def __init__(self, keyword, path, verbose):
         self.keyword = keyword
         self.path = path
+        self.verbose = verbose
 
 @click.command()
 @click.option("-k", "--keyword", type=str, help = "Keyword to categorize files", required=True)
 @click.option("-p", "--path", type=str, help = "Path to organize", required=False, default=os.getcwd())
+@click.option("-v", "--verbose", is_flag = True, help = "Show complete output", required = False, default = False)
 @click.pass_context
-def main(ctx, keyword, path):
+def main(ctx, keyword, path, verbose):
     """Organize files based on specified keyword"""
-    ctx.obj = Context(keyword, path)
+    ctx.obj = Context(keyword, path, verbose)
     keyword = ctx.obj.keyword
     folder_to_track = ctx.obj.path
+    verbose = ctx.obj.verbose
 
     if os.path.exists(folder_to_track):
-        click.echo(specific_name_category(keyword, folder_to_track))
-    else:
-        click.echo("{}: does not exist".format(folder_to_track))
+        click.echo(specific_name_category(keyword, folder_to_track, verbose))
+    elif not os.path.exists(folder_to_track):
+        click.echo(os.linesep + Fore.RED + "Error: {} does not exist".format(folder_to_track) + os.linesep)
     
+  
