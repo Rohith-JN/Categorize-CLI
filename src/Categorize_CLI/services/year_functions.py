2259fcb9dcc41074f8af7b8190d7c8c1bc4b9018
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-28 16:13:43 +0530

    Added verbose output

--- src/Categorize_CLI/services/year_functions.py
+++ src/Categorize_CLI/services/year_functions.py
@@ -1,11 +1,18 @@
 import os
 import time
 from datetime import timedelta
+from colorama import init
+from colorama import Fore
 
 from src.Categorize_CLI.common.secondary_functions import *
 
-def year_category(folder_to_track):
+init()
+
+def year_category(folder_to_track, verbose):
     start_time = time.monotonic()
+    moved = {}
+    movedList = []
+    no_of_files_moved = 0
     size = 0
     movedFiles = False
     years = set()
@@ -35,6 +42,7 @@ def year_category(folder_to_track):
                                 source = os.path.join(folder_to_track, filename)
                                 size = size + os.path.getsize(source)
                                 destination = os.path.join(folder_path, filename)
+                                moved[filename] = folder_name + '/' + filename
                                 moveIncrementing(source, destination)
                                 movedFiles = True
 
@@ -49,22 +57,40 @@ def year_category(folder_to_track):
                                 source = os.path.join(folder_to_track, filename)
                                 size = size + os.path.getsize(source)
                                 destination = os.path.join(folder_path, filename)
+                                moved[filename] = folder_name + '/' + filename
                                 moveIncrementing(source, destination)
                                 movedFiles = True
 
             end_time = time.monotonic()
 
-            displayProgressbar(count)
+            for key, value in moved.items():
+                no_of_files_moved = no_of_files_moved + 1
+                movedList.append(f"{no_of_files_moved}) {key} {Fore.GREEN} --> {Fore.WHITE} {value}")
+
+            output = '\n'.join(map(str, movedList))
 
             if movedFiles:
-                if count == 1:
-                    return f"Successfully moved {count} file{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}"
+                displayProgressbar(count)
+                if count == 1 and verbose:
+                    return f"Successfully moved {count} file{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}{os.linesep}Files Moved:{os.linesep}{output}{os.linesep}"
+                elif count != 1 and verbose:
+                    return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}{os.linesep}Files Moved:{os.linesep}{output}{os.linesep}"
+                elif count == 1 and not verbose:
+                    return f"Successfully moved {count} file{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
+                elif count != 1 and not verbose:
+                    return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
                 else:
-                    return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}"
+                    return f"Successfully moved {count} files{os.linesep}Time taken: {timedelta(seconds=end_time - start_time)}{os.linesep}Total size of files moved: {calc_size(size)}{os.linesep}"
+
             else:
-                return f'{folder_to_track}: is either empty or not organizable'
+                return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
 
         except Exception as e:
-            return f'{folder_to_track}: is either empty or not organizable'
-    else:
-        return f'{folder_to_track}: is either empty or not organizable'
+            return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
+
+    elif os.listdir(folder_to_track).__len__() == 0:
+        return os.linesep + Fore.RED + f'Error: {folder_to_track} is empty' + os.linesep
+    elif all(os.path.isdir(os.path.join(folder_to_track, file + '//')) for file in os.listdir(folder_to_track)):
+        return os.linesep + Fore.RED + f'Error: {folder_to_track} only contains folders' + os.linesep
+    else: 
+        return os.linesep + Fore.RED + f'Error: Could not organize {folder_to_track}' + os.linesep
