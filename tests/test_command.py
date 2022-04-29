f6b55e3c2dcdda8a864d2f550d8c5f0d8d3826cb
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-04-29 20:45:49 +0530

    Added test

---
+++ tests/test_command.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def test_command():
+    try:
+        result = subprocess.run(['Categorize'], check=True, capture_output=True, text=True, shell=True)
+    except subprocess.CalledProcessError as error:
+        print(error.stdout)
+        print(error.stderr)
+        raise error
+
+if __name__ == '__main__':
+    test_command()
